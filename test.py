91bab14a660bdca9c68f9ec150bb858dafb44192
Fsoky <cyberuest0x12@gmail.com>
2021-04-19 00:43:54 +0600

    test longpoll

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from vkturbo.vkturbo import VkApi
 from vkturbo.handler import EventHandler
+from vkturbo.longpoll import LongPoll, EventType
 
 vk = VkApi("token")
 handler = EventHandler(vk)
@@ -9,3 +10,22 @@ handler = EventHandler(vk)
 async def get_status():
 	status = await vk.method("status.get", {"user_id": vk.user_id("ansqqq")})
 	print(status)
+
+
+@handler.event()
+async def test_longpoll():
+	async for event in await LongPoll(vk).listen():
+		if event[0] == EventType.MESSAGE_NEW: # or 4
+			msg = event[5]
+			user_id = event[3]
+
+			if msg == "test":
+				await vk.method("messages.send",
+					{
+						"user_id": user_id,
+						"message": "test message",
+						"random_id": 0
+					}
+				)
+
+	# There bug... ;3
