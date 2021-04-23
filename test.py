9d74f054d141e4f9aabf51822eed5b47498b8444
Fsoky <cyberuest0x12@gmail.com>
2021-04-23 22:48:29 +0600

    new test

--- test.py
+++ test.py
@@ -5,22 +5,22 @@ from vkturbo.keyboard import Keyboard, KeyboardButton
 
 vk = VkTurbo("TOKEN")
 longpoll = LongPoll(vk)
-handler = EventHandler()
+handler = EventHandler(vk, longpoll) # If you wanna use just methods: longpoll=None
 
 
 """
 @handler.handler
 async def simple_using_method():
-	await vk.method("status.get", {"user_id": vk.user_id("ansqqq")})
+	await vk.method("status.get", {"user_id": vk.get_id("ansqqq")})
 """
 
 # LongPoll...
 @handler.event()
 async def test_keyboard():
 	async for event in await longpoll.listen():
-		if event[0] == 4:
-			user_id = event[3]
-			text = event[5].lower()
+		if event.type == EventType.MESSAGE_NEW:
+			user_id = event.user_id
+			text = event.text.lower()
 
 			if text == "test":
 				keyboard = Keyboard(
